20227a953389ccfbe544dc40e327a5339a25e4f3
verdienansein <gugli90@hotmail.com>
2026-03-09 14:02:02 +0100

    feat: refactor good affixes handling in ManualEvaluator for improved clarity

--- d2r_analyzer/evaluator/manual_evaluation.py
+++ d2r_analyzer/evaluator/manual_evaluation.py
@@ -4,33 +4,36 @@ class ManualEvaluator:
 
     def evaluate_item(self, item: dict) -> dict:
         evaluation = {}
+        good_affixes = []
         score = 0
         if item["quality"] == "unique":
             evaluation["verdict"] = "KEEP"
             evaluation["grade"] = "S"
+            evaluation["good_affixes"] = good_affixes
+            return evaluation
         if item["quality"] == "magic":
             for affix in item["affixes"]:
                 if affix.get("stat") == "faster_cast_rate" and affix.get("value") >= 10:
                     score += (affix.get("value") / 20) * 50
-                    evaluation["good_affixes"] = evaluation.get("good_affixes", []) + [affix.get("raw_text")]
+                    good_affixes.append(affix.get("raw_text"))
                 if affix.get("stat") == "all_resistances" and affix.get("value") >= 10:
                     score += (affix.get("value") / 30) * 50
-                    evaluation["good_affixes"] = evaluation.get("good_affixes", []) + [affix.get("raw_text")]
+                    good_affixes.append(affix.get("raw_text"))
                 if (
                     affix.get("stat") == "better_chance_of_getting_magic_items"
                     and affix.get("value") >= 25
                 ):
                     score += (affix.get("value") / 40) * 100
-                    evaluation["good_affixes"] = evaluation.get("good_affixes", []) + [affix.get("raw_text")]
+                    good_affixes.append(affix.get("raw_text"))
                 if (
                     (affix.get("stat") == "lightning_skills" or affix.get("stat") == "fire_skills" or affix.get("stat") == "cold_skills")
                     and affix.get("value") >= 1
                 ):
                     score += (affix.get("value") / 3) * 50
-                    evaluation["good_affixes"] = evaluation.get("good_affixes", []) + [affix.get("raw_text")]
+                    good_affixes.append(affix.get("raw_text"))
                 if affix.get("stat") == "life" and affix.get("value") >= 90:
                     score += (affix.get("value") / 100) * 50
-                    evaluation["good_affixes"] = evaluation.get("good_affixes", []) + [affix.get("raw_text")]
+                    good_affixes.append(affix.get("raw_text"))
 
         if score >= 75:
             evaluation["verdict"] = "KEEP"
@@ -45,4 +48,5 @@ class ManualEvaluator:
             evaluation["verdict"] = "DISCARD"
             evaluation["grade"] = "C"
 
+        evaluation["good_affixes"] = good_affixes
         return evaluation
